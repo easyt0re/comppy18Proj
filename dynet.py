a482ec21b9a9e388ebfb210f93646394c3192ef0
Unknown <imlsnapywy@gmail.com>
2018-10-30 21:17:03 +0100

    added .gitignore and main code

---
+++ dynet.py
@@ -0,0 +1,195 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self, inputDimension=12):
+        super(Net, self).__init__()
+        # input is a vector of a certain dimension
+        # TODO: input is supposed to be a batch
+        # kernel
+        # hyper params
+        nFeatL1 = inputDimension * inputDimension
+        nFeatL2 = nFeatL1
+        nFeatL3 = nFeatL1
+        outputDimension = 6
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(inputDimension, nFeatL1)
+        self.fc2 = nn.Linear(nFeatL1, nFeatL2)
+        self.fc3 = nn.Linear(nFeatL2, nFeatL3)
+        self.fc4 = nn.Linear(nFeatL3, outputDimension)
+
+    def forward(self, x):
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+net = Net()
+print(net)
+
+########################################################################
+# You just have to define the ``forward`` function, and the ``backward``
+# function (where gradients are computed) is automatically defined for you
+# using ``autograd``.
+# You can use any of the Tensor operations in the ``forward`` function.
+#
+# The learnable parameters of a model are returned by ``net.parameters()``
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())  # conv1's .weight
+
+########################################################################
+# Let try a random 32x32 input
+# Note: Expected input size to this net(LeNet) is 32x32. To use this net on
+# MNIST dataset, please resize the images from the dataset to 32x32.
+
+input = torch.randn(1, 1, 12, 1)
+out = net(input)
+print(out)
+
+########################################################################
+# Zero the gradient buffers of all parameters and backprops with random
+# gradients:
+net.zero_grad()
+out.backward(torch.randn(1, 6))
+
+########################################################################
+# .. note::
+#
+#     ``torch.nn`` only supports mini-batches. The entire ``torch.nn``
+#     package only supports inputs that are a mini-batch of samples, and not
+#     a single sample.
+#
+#     For example, ``nn.Conv2d`` will take in a 4D Tensor of
+#     ``nSamples x nChannels x Height x Width``.
+#
+#     If you have a single sample, just use ``input.unsqueeze(0)`` to add
+#     a fake batch dimension.
+#
+# Before proceeding further, let's recap all the classes you’ve seen so far.
+#
+# **Recap:**
+#   -  ``torch.Tensor`` - A *multi-dimensional array* with support for autograd
+#      operations like ``backward()``. Also *holds the gradient* w.r.t. the
+#      tensor.
+#   -  ``nn.Module`` - Neural network module. *Convenient way of
+#      encapsulating parameters*, with helpers for moving them to GPU,
+#      exporting, loading, etc.
+#   -  ``nn.Parameter`` - A kind of Tensor, that is *automatically
+#      registered as a parameter when assigned as an attribute to a*
+#      ``Module``.
+#   -  ``autograd.Function`` - Implements *forward and backward definitions
+#      of an autograd operation*. Every ``Tensor`` operation, creates at
+#      least a single ``Function`` node, that connects to functions that
+#      created a ``Tensor`` and *encodes its history*.
+#
+# **At this point, we covered:**
+#   -  Defining a neural network
+#   -  Processing inputs and calling backward
+#
+# **Still Left:**
+#   -  Computing the loss
+#   -  Updating the weights of the network
+#
+# Loss Function
+# -------------
+# A loss function takes the (output, target) pair of inputs, and computes a
+# value that estimates how far away the output is from the target.
+#
+# There are several different
+# `loss functions <http://pytorch.org/docs/nn.html#loss-functions>`_ under the
+# nn package .
+# A simple loss is: ``nn.MSELoss`` which computes the mean-squared error
+# between the input and the target.
+#
+# For example:
+
+output = net(input)
+target = torch.randn(6)  # a dummy target, for example
+target = target.view(1, -1)  # make it the same shape as output
+criterion = nn.MSELoss()
+
+loss = criterion(output, target)
+print(loss)
+
+########################################################################
+# Backprop
+# --------
+# To backpropagate the error all we have to do is to ``loss.backward()``.
+# You need to clear the existing gradients though, else gradients will be
+# accumulated to existing gradients.
+#
+#
+# Now we shall call ``loss.backward()``, and have a look at conv1's bias
+# gradients before and after the backward.
+
+
+net.zero_grad()     # zeroes the gradient buffers of all parameters
+
+print('fc1.bias.grad before backward')
+print(net.fc1.bias.grad)
+
+loss.backward()
+
+print('fc1.bias.grad after backward')
+print(net.fc1.bias.grad)
+
+########################################################################
+# Now, we have seen how to use loss functions.
+#
+# **Read Later:**
+#
+#   The neural network package contains various modules and loss functions
+#   that form the building blocks of deep neural networks. A full list with
+#   documentation is `here <http://pytorch.org/docs/nn>`_.
+#
+# **The only thing left to learn is:**
+#
+#   - Updating the weights of the network
+#
+# Update the weights
+# ------------------
+# The simplest update rule used in practice is the Stochastic Gradient
+# Descent (SGD):
+#
+#      ``weight = weight - learning_rate * gradient``
+#
+# We can implement this using simple python code:
+#
+# .. code:: python
+#
+#     learning_rate = 0.01
+#     for f in net.parameters():
+#         f.data.sub_(f.grad.data * learning_rate)
+#
+# However, as you use neural networks, you want to use various different
+# update rules such as SGD, Nesterov-SGD, Adam, RMSProp, etc.
+# To enable this, we built a small package: ``torch.optim`` that
+# implements all these methods. Using it is very simple:
+
+import torch.optim as optim
+
+# create your optimizer
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+# in your training loop:
+optimizer.zero_grad()   # zero the gradient buffers
+output = net(input)
+loss = criterion(output, target)
+loss.backward()
+optimizer.step()    # Does the update
+
